5d839d8c4d4f4f5162efe014ff6b919f06c97a99
ITALO GOTHARDO TAVARES HISSA <italohissa0570@digitalcollege.internal>
2026-09-21 11:46:50 -0300

    second

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function gaveta(){
     let nav = document.querySelector("header nav");
-
+    let orverlay = document.querySelector("#overlay");
     nav.classList.add("active");
+    orverlay.classList.add("active")
+}
+
+function fechatGaveta(){
+    let nav = document.querySelector("header nav");
+    let orverlay = document.querySelector("#overlay");
+    nav.classList.remove("active");
+    orverlay.classList.remove("active")
 }
